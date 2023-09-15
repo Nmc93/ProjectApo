45d32e3e7f933771b5e129d597228ef8d0cf6ed4
seqw <shpp9574@naver.com>
2023-09-15 18:58:36 +0900

    작업
     - 계산 결과로 나온 최종 스탯 변수 추가
     - 무기 착용 기능 추가

--- Assets/Scripts/InGame/Unit/Unit.cs
+++ Assets/Scripts/InGame/Unit/Unit.cs
@@ -45,53 +45,94 @@ public class Unit : MonoBehaviour
         UnitAppearanceTableData tbl;
 
         // 머리 세팅
-        if (TableMgr.Get(data.headType,out tbl))
+        if (TableMgr.Get(data.headID,out tbl))
         {
             head.sprite = AssetsMgr.GetSprite(eAtlasType.Unit_Human, tbl.Path);
         }
 
         // 얼굴
-        if (TableMgr.Get(data.faceType, out tbl))
+        if (TableMgr.Get(data.faceID, out tbl))
         {
             face.sprite = AssetsMgr.GetSprite(eAtlasType.Unit_Human, tbl.Path);
         }
 
         //얼굴 장식
-        if (TableMgr.Get(data.faceDecoType, out tbl))
+        if (TableMgr.Get(data.faceDecoID, out tbl))
         {
             faceDeco.sprite = AssetsMgr.GetSprite(eAtlasType.Unit_Human, tbl.Path);
         }
         faceDeco.gameObject.SetActive(tbl != null);
 
         //머리카락 세팅
-        if (TableMgr.Get(data.hairType, out tbl))
+        if (TableMgr.Get(data.hairID, out tbl))
         {
             hair.sprite = AssetsMgr.GetSprite(eAtlasType.Unit_Human, tbl.Path);
         }
         hair.gameObject.SetActive(tbl != null);
 
         //뒷머리 세팅
-        if (TableMgr.Get(data.backHairType, out tbl))
+        if (TableMgr.Get(data.backHairID, out tbl))
         {
             backHair.sprite = AssetsMgr.GetSprite(eAtlasType.Unit_Human, tbl.Path);
         }
         backHair.gameObject.SetActive(tbl != null);
 
         //모자 세팅
-        if (TableMgr.Get(data.hatType, out tbl))
+        if (TableMgr.Get(data.hatID, out tbl))
         {
             hat.sprite = AssetsMgr.GetSprite(eAtlasType.Unit_Human, tbl.Path);
         }
         hat.gameObject.SetActive(tbl != null);
 
-        //몸, 팔 세팅 (애니메이션 컨트롤러)
-        animator.runtimeAnimatorController = AssetsMgr.GetUnitRuntimeAnimatorController(data.bodyType);
-
-        //무기 세팅
-        if (TableMgr.Get(data.weaponType, out tbl))
+        //무기 세팅(맨손일 경우 세팅하지 않음)
+        if(data.weaponTbl.Path == "None")
         {
-            weapon.sprite = AssetsMgr.GetSprite(eAtlasType.Unit_Human, tbl.Path);
+            weapon.sprite = AssetsMgr.GetSprite(eAtlasType.Unit_Human, data.weaponTbl.Path);
+            weapon.gameObject.SetActive(true);
         }
-        weapon.gameObject.SetActive(tbl != null);
+        else
+        {
+            weapon.gameObject.SetActive(false);
+        }
+
+        //몸, 팔 세팅 (애니메이션 컨트롤러)
+        animator.runtimeAnimatorController = AssetsMgr.GetUnitRuntimeAnimatorController(data.bodyID);
+    }
+
+    public void ChangeSprite(Sprite sprite, UnitAppearanceTableData tbl)
+    {
+        //테이블이 없을 경우 종료함
+        if(tbl == null)
+        {
+            return;
+        }
+
+        //이미지 및 애니메이션 변경
+        sprite = AssetsMgr.GetSprite(eAtlasType.Unit_Human, tbl.Path);
+    }
+
+    /// <summary> 무기 변경 </summary>
+    /// <param name="weaponID"> 무기의 ID </param>
+    public void ChangeWeapon(int weaponID)
+    {
+        //무기 정보 변경
+        data.SetWeaponData(weaponID);
+
+        //이미지 및 애니메이션 변경
+        if (data.weaponTbl.Path == "None")
+        {
+            weapon.sprite = AssetsMgr.GetSprite(eAtlasType.Unit_Human, data.weaponTbl.Path);
+            weapon.gameObject.SetActive(true);
+        }
+        else
+        {
+            weapon.gameObject.SetActive(false);
+        }
+    }
+
+    /// <summary> 캐릭터 스탯 계산 </summary>
+    private void RefreshStat()
+    {
+        data.RefreshStat();
     }
 }
--- Assets/Scripts/InGame/Unit/UnitData.cs
+++ Assets/Scripts/InGame/Unit/UnitData.cs
@@ -10,19 +10,21 @@ public class UnitData
 {
     public UnitData(
         int unitType,
-        int headType,
-        int hatType,
-        int hairType,
-        int backHairType,
-        int faceType,
-        int faceDecoType,
-        int bodyType,
+        int headID,
+        int hatID,
+        int hairID,
+        int backHairID,
+        int faceID,
+        int faceDecoID,
+        int bodyID,
         int maxHp,
         int attack,
         int defence,
         float attackSpeed,
-        float moveSpeed)
+        float moveSpeed,
+        int weaponID = 0)
     {
+        //유닛의 타입 세팅
         switch(unitType)
         {
             case 0: this.unitType = eUnitType.None; break;
@@ -30,69 +32,114 @@ public class UnitData
             case 2: this.unitType = eUnitType.Zombie; break;
         }
 
-        this.headType = headType;
-        this.hatType = hatType;
-        this.hairType = hairType;
-        this.hairType = hairType;
-        this.backHairType = backHairType;
-        this.faceType = faceType;
-        this.faceDecoType = faceDecoType;
-        this.bodyType = bodyType;
+        //외형 세팅
+        this.headID = headID;
+        this.hatID = hatID;
+        this.hairID = hairID;
+        this.hairID = hairID;
+        this.backHairID = backHairID;
+        this.faceID = faceID;
+        this.faceDecoID = faceDecoID;
+        this.bodyID = bodyID;
+        
+        //베이스 스탯 세팅
         this.maxHp = maxHp;
         this.attack = attack;
         this.defence = defence;
         this.attackSpeed = attackSpeed;
         this.moveSpeed = moveSpeed;
+
+        //무기 세팅
+        weaponTbl = TableMgr.Get<UnitWeaponTableData>(weaponID);
+
+        //스탯 갱신
+        RefreshStat();
     }
 
     /// <summary> 무기 데이터 세팅 </summary>
     public void SetWeaponData(int weaponID)
     {
-
+        //무기 슬롯이 비어있거나 교체 무기가 현재와 다를 경우 변경
+        if (weaponTbl != null || weaponTbl.ID != weaponID)
+        {
+            //정보 변경 및 무기 변경으로 인한 스탯 갱신
+            weaponTbl = TableMgr.Get<UnitWeaponTableData>(weaponID);
+            RefreshStat();
+        }
     }
 
     /// <summary> 유닛 타입 </summary>
     public eUnitType unitType;
 
-    /// <summary> 무기 타입 </summary>
-    public eWeaponType weaponType;
-    /// <summary> 무기 ID </summary>
-    public int weaponID;
+    /// <summary> 무기의 테이블 정보 </summary>
+    public UnitWeaponTableData weaponTbl;
     
     /// <summary> 유닛 이름 </summary>
     public string name;
 
     #region 외형 정보
     /// <summary> 머리 타입 </summary>
-    public int headType;
+    public int headID;
     /// <summary> 모자 타입 </summary>
-    public int hatType;
+    public int hatID;
     /// <summary> 머리카락 타입 </summary>
-    public int hairType;
+    public int hairID;
     /// <summary> 뒷머리 타입 </summary>
-    public int backHairType;
+    public int backHairID;
     /// <summary> 얼굴 타입 </summary>
-    public int faceType;
+    public int faceID;
     /// <summary> 얼굴 장식 타입(콧수염 등) </summary>
-    public int faceDecoType;
+    public int faceDecoID;
     /// <summary> 몸 타입 </summary>
-    public int bodyType;
+    public int bodyID;
     #endregion 외형 정보 
 
     #region 스텟 정보
-    /// <summary> 유닛 최대 체력 </summary>
+
+    #region 기본 스탯
+    /// <summary> 유닛 체력 </summary>
     public int maxHp;
-    /// <summary> 유닛 현재 체력 </summary>
-    public int curHp;
 
     /// <summary> 유닛 공격력 </summary>
     public int attack;
     /// <summary> 유닛 방어력 </summary>
     public int defence;
 
-    /// <summary> 유닛의 공격 속도 </summary>
+    /// <summary> 유닛의 공격속도 </summary>
     public float attackSpeed;
-    /// <summary> 유닛의 이동 속도 </summary>
+    /// <summary> 유닛의 이동속도 </summary>
     public float moveSpeed;
+    #endregion 기본 스탯
+
+    #region 적용 스탯
+    #endregion 적용 스탯
+
+    /// <summary> 유닛 현재 체력 </summary>
+    public int curMaxHp;
+    /// <summary> 유닛 현재 체력 </summary>
+    public int curHp;
+    /// <summary> 현재 공격력 </summary>
+    public int dmg;
+    /// <summary> 현재 방어력 </summary>
+    public int def;
+
+    /// <summary> 현재 공격속도 </summary>
+    public float aSpeed;
+    /// <summary> 현재 이동속도 </summary>
+    public float mSpeed;
+
     #endregion 스텟 정보
+
+    /// <summary> 현재 기본 스탯을 기반으로 스탯 계산 </summary>
+    public void RefreshStat()
+    {
+        //체력 세팅
+        curHp = curMaxHp = maxHp;
+
+        dmg = attack;// + 총기 데미지
+        def = defence;// + 추가적인 무언가
+
+        aSpeed = attackSpeed; // 장비 특성에 따라 비율 조절 추가
+        mSpeed = moveSpeed; // 장비 특성에 따라 비율 조절 추가
+    }
 }
